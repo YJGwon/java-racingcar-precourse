15679d7183f7f85d1ca4d5d1b03badaddb7760a5
YJGwon <yj970125@gmail.com>
2021-12-16 17:23:37 +0900

    feat: [예외] 이름이 공백이 아닌지 검증해야 한다

--- src/main/java/racingcar/InputValidator.java
+++ src/main/java/racingcar/InputValidator.java
@@ -12,19 +12,29 @@ public class InputValidator {
 	private static final int MAX_NAME_LENGTH = 5;
 
 	private static final String NAME_LENGTH_ERROR_MESSAGE = "이름은 5자를 넘을 수 없다.";
+	private static final String BLANK_ERROR_MESSAGE = "이름은 공백일 수 없다.";
+
 	public String[] validateCarNames(String input) {
 		String[] carNames = input.split(CAR_NAME_SEPARATOR);
 		for (String carName : carNames) {
 			carName = carName.trim();
 			checkNameLength(carName);
+			checkBlank(carName);
 		}
 		return carNames;
 	}
 
 	private void checkNameLength(String carName) {
-		if(carName.length() <= MAX_NAME_LENGTH) {
+		if (carName.length() <= MAX_NAME_LENGTH) {
 			return;
 		}
 		throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+	}
+
+	private void checkBlank(String carName) {
+		if (carName.length() > 0) {
+			return;
+		}
+		throw new IllegalArgumentException(BLANK_ERROR_MESSAGE);
 	}
 }
