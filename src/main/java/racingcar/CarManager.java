69ecae70ce90b15d79c7495b273aa6d807c23ea0
YJGwon <yj970125@gmail.com>
2021-12-16 17:47:34 +0900

    fix: NullPointerException 해결
    CarManager.cars에 대해 stream 생성 전 초기화 먼저 하도록 수정

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -12,24 +12,23 @@ import java.util.ArrayList;
 public class CarManager {
 	private static final String NAME_DUPLICATE_ERROR_MESSAGE = "중복된 이름이 있습니다.";
 
-	private ArrayList<Car> cars;
+	private final ArrayList<Car> cars = new ArrayList<>();
 
 	public CarManager(String[] carNames) {
 		lineUpCars(carNames);
 	}
 
 	private void lineUpCars(String[] carNames) {
-		ArrayList<Car> cars = new ArrayList<>();
 		for (String carName : carNames) {
 			checkNameDuplicate(carName);
 			cars.add(new Car(carName));
 		}
-		this.cars = cars;
 	}
 
 	private void checkNameDuplicate(String carName) {
-		if (cars.stream().anyMatch(car -> car.isName(carName))) {
-			throw new IllegalArgumentException(NAME_DUPLICATE_ERROR_MESSAGE);
+		if (cars.stream().noneMatch(car -> car.isName(carName))) {
+			return;
 		}
+		throw new IllegalArgumentException(NAME_DUPLICATE_ERROR_MESSAGE);
 	}
 }
