6cd62ecc4171dba6a56cd169c9a9240481dfd820
YJGwon <yj970125@gmail.com>
2021-12-16 17:38:26 +0900

    feat: [예외] 이름이 중복되지 않았는지 검증해야 한다

--- src/main/java/racingcar/CarManager.java
+++ src/main/java/racingcar/CarManager.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
  * @version 1.0
  */
 public class CarManager {
+	private static final String NAME_DUPLICATE_ERROR_MESSAGE = "중복된 이름이 있습니다.";
+
 	private ArrayList<Car> cars;
 
 	public CarManager(String[] carNames) {
@@ -19,8 +21,15 @@ public class CarManager {
 	private void lineUpCars(String[] carNames) {
 		ArrayList<Car> cars = new ArrayList<>();
 		for (String carName : carNames) {
+			checkNameDuplicate(carName);
 			cars.add(new Car(carName));
 		}
 		this.cars = cars;
+	}
+
+	private void checkNameDuplicate(String carName) {
+		if (cars.stream().anyMatch(car -> car.isName(carName))) {
+			throw new IllegalArgumentException(NAME_DUPLICATE_ERROR_MESSAGE);
+		}
 	}
 }
